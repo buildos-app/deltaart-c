80d9ea853d4195a8c00abd92b1a89627451ba7bc
buildos-app <mattarahmed112@gmail.com>
2026-06-14 21:51:14 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,18 @@
-const CACHE = 'deltaart-v1';
-const ASSETS = ['/', '/index.html'];
+const CACHE_NAME = 'deltaart-v1';
+const STATIC_ASSETS = ['/', '/index.html'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{})
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(STATIC_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
   );
